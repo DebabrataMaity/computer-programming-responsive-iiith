224162fca6aefbbf385b925800d8aec1ecf01e7b
DebabrataMaity <46934864+DebabrataMaity@users.noreply.github.com>
2020-07-02 19:19:04 +0530

    Update array.js

--- src/lab/exp4/simulation/2-D/array.js
+++ src/lab/exp4/simulation/2-D/array.js
@@ -119,10 +119,12 @@ window.view = {
 	},
 	generateFirstMatrixElements: function() {
 		var size = this.rowsA * this.colsA
+		var k=0
 		for ( i = 0 ; i < size ; i++)
 		{
-			var random = Math.floor(Math.random()*15)
+			var random = k;
 			this.matrixA.push(random)
+			k++;
 		}
 		this.resetRowsAndCols()
 		this.disableButton('row')
@@ -135,10 +137,12 @@ window.view = {
 	},
 	generateSecondMatrixElements: function() {
 		var size = this.rowsB * this.colsB
+		var l=0;
 		for ( i = 0 ; i < size ; i++)
 		{
-			var random = Math.floor(Math.random()*15)
+			var random = l;
 			this.matrixB.push(random)
+			l++;
 		}
 		this.disableButton('generateB')
 		this.changeClass( 'generateB', 'buttonDisable GenerateValueButton show' )
